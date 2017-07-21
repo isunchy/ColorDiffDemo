75a2ab76eb24f0ec0fd413028d3a44dc98f67080
unknown <milkpku@gmail.com>
2017-07-21 15:14:17 +0800

    fix get_compress_density for loop range

--- get_compress_density.m
+++ get_compress_density.m
@@ -1,10 +1,11 @@
 %% Function: get compress density
 function density_compress = get_compress_density(density)
     density_compress = zeros(nnz(density), 4);
+    [li, lj, lk] = size(density);
     count = 1;
-    for i=1:256
-        for j=1:256
-            for k=1:256
+    for i=1:li
+        for j=1:lj
+            for k=1:lk
                 if density(i,j,k)~=0
                     density_compress(count,:) = [i,j,k,density(i,j,k)];
                     count =count + 1;
